e6a7363d15ff4002956facf6ac1bb2b82dcabfa5
Antonio de las Morenas <a.j.delasmorenas@gmail.com>
2019-09-18 00:04:14 -0500

    Added LIRI Bot to my portfolio

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -8,7 +8,13 @@ $(document).ready(function () {
 
         $("#dynamic-title").text(projName);
 
-        $("#dynamic-description").html("<strong>Deployed App:</strong> <a href='" + projDeployed + "' target='_blank'>" + projDeployed + "</a><br /><strong>GitHub Repository:</strong> <a href='" + projRepo + "' target='_blank'>" + projRepo + "</a>");
+        if (projDeployed === "not available") {
+            $("#dynamic-description").html("<strong>GitHub Repository:</strong> <a href='" + projRepo + "' target='_blank'>" + projRepo + "</a>");
+        }
+
+        else {
+            $("#dynamic-description").html("<strong>Deployed App:</strong> <a href='" + projDeployed + "' target='_blank'>" + projDeployed + "</a><br /><strong>GitHub Repository:</strong> <a href='" + projRepo + "' target='_blank'>" + projRepo + "</a>");
+        };
 
         $("#project-info").modal("show");
     }
@@ -19,49 +25,49 @@ $(document).ready(function () {
         displayModal($(this).attr("data-name"), $(this).attr("data-deployed"), $(this).attr("data-repo"));
     });
 
-    $(".fa-home").mouseover(function() {
+    $(".fa-home").mouseover(function () {
         $(".fa-home").addClass("d-none");
 
         $(".aboutFont").removeClass("d-none").addClass("d-inline");
     });
 
-    $(".aboutFont").mouseout(function() {
+    $(".aboutFont").mouseout(function () {
         $(".aboutFont").removeClass("d-inline").addClass("d-none");
 
         $(".fa-home").removeClass("d-none");
     });
 
-    $(".fa-folder-open").mouseover(function() {
+    $(".fa-folder-open").mouseover(function () {
         $(".fa-folder-open").addClass("d-none");
 
         $(".portfolioFont").removeClass("d-none").addClass("d-inline");
     });
 
-    $(".portfolioFont").mouseout(function() {
+    $(".portfolioFont").mouseout(function () {
         $(".portfolioFont").removeClass("d-inline").addClass("d-none");
 
         $(".fa-folder-open").removeClass("d-none");
     });
 
-    $(".fa-phone").mouseover(function() {
+    $(".fa-phone").mouseover(function () {
         $(".fa-phone").addClass("d-none");
 
         $(".contactFont").removeClass("d-none").addClass("d-inline");
     });
 
-    $(".contactFont").mouseout(function() {
+    $(".contactFont").mouseout(function () {
         $(".contactFont").removeClass("d-inline").addClass("d-none");
 
         $(".fa-phone").removeClass("d-none");
     });
 
-    $(".fa-file").mouseover(function() {
+    $(".fa-file").mouseover(function () {
         $(".fa-file").addClass("d-none");
 
         $(".resumeFont").removeClass("d-none");
     });
 
-    $(".resumeFont").mouseout(function() {
+    $(".resumeFont").mouseout(function () {
         $(".resumeFont").addClass("d-none");
 
         $(".fa-file").removeClass("d-none");
